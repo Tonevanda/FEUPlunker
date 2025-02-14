b9ed38a1dc343e77c6f0287009992011c58ed6ff
Tomás Xavier <tomasxavier03@gmail.com>
2025-02-14 18:10:19 +0000

    trying fixes on animation state tree

--- feuplunker/player.gd
+++ feuplunker/player.gd
@@ -16,14 +16,20 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+		
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
-	animationTree.
 	if direction:
 		velocity.x = direction * SPEED
+		animationTree.set("parameters/conditions/Running",true)
+		animationTree.set("parameters/conditions/Idling",false)
+		animationTree["parameters/RunBlend/blend_position"] = direction
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+		animationTree.set("parameters/conditions/Idling",true)
+		animationTree.set("parameters/conditions/Running",false)
+		animationTree["parameters/IdleBlend/blend_position"] = 1
 
 	move_and_slide()
