2c46612ad39753fd350ce3ac0fffb2c8ef326baf
dratomitoma <64918169+dratomitoma@users.noreply.github.com>
2025-02-18 13:30:18 +0000

    started jump

--- feuplunker/player.gd
+++ feuplunker/player.gd
@@ -15,22 +15,22 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
 		animationTree["parameters/conditions/Idling"] = false
+		animationTree["parameters/conditions/Jumping"] = false
 		animationTree["parameters/conditions/Running"] = true
-		#animationTree.get("parameters/playback").travel("RunBlend")
 		animationTree["parameters/RunBlend/blend_position"] = direction
 		animationTree["parameters/IdleBlend/blend_position"] = direction
+		animationTree["parameters/JumpBlend/blend_position"] = direction
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		#animationTree.get("parameters/playback").travel("IdleBlend")
 		animationTree["parameters/conditions/Idling"] = true
 		animationTree["parameters/conditions/Running"] = false
 		
