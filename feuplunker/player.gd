2ada7cd84ea0af14403c7bd0af0377f7aa1fdbd1
Tonevanda <up202108863@up.pt>
2025-03-07 15:30:19 +0000

    adjusted movement values and level stuff

--- feuplunker/player.gd
+++ feuplunker/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 150.0
+const JUMP_VELOCITY = -300.0
 @export var downAttackSpeed = 50
 @onready var animationPlayer = get_node("AnimationPlayer")
 @onready var animationTree = get_node("AnimationTree")
