0dae71f6b91c1f944401ac17c0c42c1b6722fda5
Tonevanda <jmguellourenco@gmail.com>
2025-03-14 21:31:23 +0000

    changed jump velocity

--- feuplunker/scripts/player.gd
+++ feuplunker/scripts/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 var SPEED = 125.0
-const JUMP_VELOCITY = -300.0
+const JUMP_VELOCITY = -360.0
 @export var downAttackSpeed = 50
 @onready var animationPlayer = get_node("AnimationPlayer")
 @onready var animationTree = get_node("AnimationTree")
