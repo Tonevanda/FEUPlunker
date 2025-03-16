a5724dda5dec370375f18d157bca9f652b184190
Tonevanda <jmguellourenco@gmail.com>
2025-03-16 13:02:48 +0000

    game over when energy gets to 0

--- feuplunker/scripts/ui.gd
+++ feuplunker/scripts/ui.gd
@@ -1,25 +1,22 @@
 extends Control
 @onready var healthBar = $HealthBar
 @onready var energyBar = $EnergyBar
+const HEALTH_DECAY = 1
+const ENERGY_DECAY = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	energyBar.value -= 1
-	if(energyBar.value <= 0):
-		# Show game over screen
-		print("death")
-	pass
+	energyBar.value -= ENERGY_DECAY
 	
 func damage():
-	healthBar.value -= 1
-	if(healthBar.value <= 0):
-		# Show game over screen
-		print("fesu")
+	healthBar.value -= HEALTH_DECAY
 
 func update_energy(value):
 	energyBar.value += value
+
+func get_energy():
+	return energyBar.value
